147f67cfa64e86528e4c952ac27ef01a4efc4c5e
S L DANAKUMAR <109226441+danakumarsl@users.noreply.github.com>
2024-10-08 15:50:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello danakumar, Docker World!"
+    message = "Hello, Docker World!"
+    print(message)  # Print to the console
+    return message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
